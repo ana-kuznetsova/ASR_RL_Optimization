914aa0e27d0c8bca77867e54f4541cd801ef3008
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-04-09 23:05:28 -0400

    [feat] add reward rescale

--- bandits.py
+++ bandits.py
@@ -10,15 +10,30 @@ class Bandit:
     def erase_rhist(self):
         self.reward_hist = []
         
-    def calc_reward(self, losses, gain='PG'):
+    def calc_reward(self, losses, gain='PG', prev_task_ind=None):
         
         if gain=='PG':
             L = losses[0]- losses[1]
-        elif gain=='SPG':
-            pass
             
-        
+        elif gain=='SPG':
+            same_task_batch = self.sample_task(prev_task_ind, same=True)
+            #Call training function here
+            L0 = losses[0]
+            L1 = train()
+            l = L0-L1
+            
         self.reward_hist.append(L)
+        
+        ##Scale reward
+        q_lo = np.ceil(np.quantile(self.reward_hist, 0.2))
+        q_hi = np.ceil(np.quantile(self.reward_hist, 0.8))
+        
+        if L < q_lo:
+            return -1
+        elif L > q_hi:
+            return 1
+        else:
+            return (2*(r-q_lo)/(q_hi-q_lo))-1  
         
     def sample_task(self, prev_task_ind, same=False):
         if same:
