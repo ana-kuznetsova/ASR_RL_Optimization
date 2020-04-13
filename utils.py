58a27224059e8e2f4ab1e115047e73b281456cbc
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-04-13 00:38:38 -0400

    Add training funciton

--- utils.py
+++ utils.py
@@ -1,6 +1,8 @@
 import pandas as pd 
+import os
+import json
 
-def save_batch(current_batch, df, path):
+def save_batch(current_batch, df, path='/N/slate/anakuzne/tatar/clips/batch.csv'):
     '''
     Saves current batch to train in DeepSpeech
     Params:
@@ -12,3 +14,21 @@ def save_batch(current_batch, df, path):
     df = train[train['wav_filename'].isin(s)]
 
     df.to_csv(path)
+
+
+def train():
+
+    os. system('cd /N/u/anakuzne/Carbonate/curr_learning/ASR_RL_Optimization/')
+    os.system('sbatch tt_train_rl.script')
+
+    with open('/N/u/anakuzne/Carbonate/curr_learning/automated_curr/loss_before.json') as f:
+        loss_before = json.load(f)
+    
+    with open('/N/u/anakuzne/Carbonate/curr_learning/automated_curr/loss_after.json') as f:
+        loss_after = json.load(f)
+
+    L1 = sum([l['loss'] for l in loss_before])/len(loss_before)
+    L2 = sum([l['loss'] for l in loss_after])/len(loss_before)
+
+    return [L1, L2]
+    
