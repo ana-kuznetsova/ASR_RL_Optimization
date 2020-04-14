bfeff914b1e36eb962bc955cb3ee1765df28d632
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-04-14 15:53:57 -0400

    upd int

--- data.py
+++ data.py
@@ -10,7 +10,7 @@ class DataSet:
     def create_tasks(self):
         
         def index_marks(nrows, chunk_size):
-            return range(chunk_size, np.ceil(nrows / chunk_size)*chunk_size, chunk_size)
+            return range(chunk_size, int(np.ceil(nrows / chunk_size)*chunk_size), chunk_size)
         
         def split(dfm, chunk_size):
             indices = index_marks(dfm.shape[0], chunk_size)
