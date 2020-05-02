7e151e262b0faa63135a7db7ce342775b7cb7326
unknown <anukr95@gmail.com>
2020-05-01 20:11:36 -0400

    Removed extra spaces

--- contextual.py
+++ contextual.py
@@ -115,8 +115,7 @@ class ContextualBandit:
             else:
                 pad = np.array(self.action_gains[a])
                 pad_len = self.dim - pad.shape[0]
-                x_t = np.pad(pad, (pad_len, 0), mode='maximum').reshape(1, -1)
-                
+                x_t = np.pad(pad, (pad_len, 0), mode='maximum').reshape(1, -1)                
             x_t = self.normalize(x_t)
             D_a = np.concatenate([D_a, x_t], axis=0)
         return D_a[1: ,  :]        
@@ -133,75 +132,55 @@ class ContextualBandit:
 
 
 def LinUCB(dataset, num_episodes, num_timesteps, batch_size, gain_type='PG'):
-    alpha = 0.05
-    
+    alpha = 0.05    
     bandit = ContextualBandit(tasks=dataset.tasks, batch_size=64, dim=5)  
-    
     for ep in range(num_episodes):
         print('-----------------------------------')
         print(f'LinUCB: Starting episode {ep+1}...')
         print('-----------------------------------')
-        
         seen = []
-        
         for t in range(num_timesteps):
             print(f'Timestep {t}')
             #Observe context for each arm, return feature matrix
             D_a = bandit.get_features()
             probs = []
-            
             for action in range(bandit.num_actions):
-                
                 if action in seen:
                     q_vals = bandit.get_qvalues()
                     A_a = q_vals['A'][str(action)]
-                    
                     b = q_vals['b'][str(action)]
                 else:
                     A_a = np.identity(bandit.dim)
                     b = np.zeros((bandit.dim,1))
                     bandit.update_qfunc(action, A_a, b)
                     seen.append(action)
-                
-                
                 x_t = D_a[action].reshape(-1,1)
                 theta = np.dot(A_a, b)
-            
                 root = alpha*np.sqrt(np.dot(np.dot(x_t.T, A_a), x_t))
                 e = np.dot(theta.T, x_t) + root
                 probs.append(float(e))
-                
             print('Probs', probs)
-                
             #Take argmax
             a_t = np.argmax(np.array(probs))
             print('Argmax action:', a_t)
             bandit.upd_action_hist(a_t)
-            
             batch = bandit.sample_task(a_t)
             save_batch(batch, 'batch_lin')
-            
             train_PG(mode='LinUCB')
             losses = load_losses_lin(feed=False)
             #losses = [700, 332]
             r = bandit.calc_reward(losses)
             print('reward:',r)
-            
             bandit.update_action_gain(a_t, r)
             x_t = D_a[a_t]
-            
             q_vals = bandit.get_qvalues()
             A_t = q_vals['A'][str(action)]
             b_t = q_vals['b'][str(action)]
-            
-            
             A_t = A_t + np.dot(x_t, x_t.T)
             x_t = x_t.reshape(-1, 1)
             b_t = b_t.reshape(-1, 1)
             b_t = b_t + r*x_t
             bandit.update_qfunc(a_t, A_t, b_t)  
-            
-            
             #Save histories to plot
             bandit.save_sc_rhist('sc_reward_hist_LinUCB.pickle')
             bandit.save_lhist('loss_hist_LinUCB.pickle')
