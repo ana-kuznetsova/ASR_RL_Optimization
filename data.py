94e7af8eaf8512c9bd8bd48bf9bf4f261d56d750
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-05-20 21:16:49 -0400

    s

--- data.py
+++ data.py
@@ -22,6 +22,6 @@ class DataSet:
         chunk_size = len(sorted_df)//self.num_tasks
         self.tasks = split(sorted_df, chunk_size)
         if len(sorted_df)%self.num_tasks != 0:
-            temp = [self.task[i] for i in range(len(self.tasks)-1)]
+            temp = [self.tasks[i] for i in range(len(self.tasks)-1)]
             temp[-1].extend(self.tasks[-1])
             self.tasks = temp
