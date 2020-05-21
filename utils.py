9fdc477fe240cb65f8cef12c6cdb417dd281da07
Anastasia Kuznetsova <menina.indigena.17@gmail.com>
2020-05-20 22:12:36 -0400

    del dir

--- utils.py
+++ utils.py
@@ -33,6 +33,7 @@ def clear_dirs(mode):
         delete_files('/N/slate/anakuzne/tt_ckpt_automated_curr/10/')
         delete_files('/N/slate/anakuzne/tt_ckpt_automated_curr/main_model/')
         delete_files('/N/u/anakuzne/Carbonate/curr_learning/automated_curr/')
+        delete_files('/N/slate/anakuzne/tt_ckpt_automated_curr/switch_task/')
     elif mode=='EXP3':
         delete_files('/N/slate/anakuzne/tt_ckpt_automated_curr/main_model_exp3/')
     elif mode=='LinUCB':
